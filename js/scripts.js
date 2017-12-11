aeec06e140d07ee662f6daa87d3f05420d277e20
Byron Chang and Jun Li <jun@>
2017-12-11 09:28:48 -0800

    converted to places youve been

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,40 @@
 //business logic
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
+function Places(location, landmarks, timeOfYear, notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
 }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+Places.prototype.printPlaces = function() {
+  return this.location + this.landmarks + this.timeOfYear + this.notes;
 }
 
 // user interface logic
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+  $("form#new-place").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedLocation = $("input#location").val();
+    var inputtedLandmarks = $("input#landmarks").val();
+    var inputtedTimeOfYear = $("input#timeOfYear").val();
+    var inputtedNotes = $("input#notes").val();
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    var newPlace = new Places(inputtedLocation, inputtedLandmarks, inputtedTimeOfYear, inputtedNotes);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("ul#places").append("<li><span class='contact'>" + newPlace.printPlaces() + "</span></li>");
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    $("input#new-location").val("");
+    $("input#new-landmarks").val("");
+    $("input#new-timeOfYear").val("");
+    $("input#new-notes").val("");
     $(".contact").last().click(function() {
-    $("#show-contact").show();
-    $("#show-contact h2").text(newContact.firstName);
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
-  });
+      $("#show-contact").show();
+      $("#show-contact h2").text(newPlace.location);
+      $(".landmarks").text(newPlace.landmarks);
+      $(".timeOfYear").text(newPlace.timeOfYear);
+      $(".notes").text(newPlace.notes);
+    });
   });
 
 });
